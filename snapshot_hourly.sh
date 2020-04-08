aa13312ae17c218c08c8b335fd189ee9933580d0
Robert Gatti <robert.gatti@gmail.com>
2020-04-07 22:22:16 -0500

    Add dry-run ability and remove fixed paths
    Refactored to be able to debug with a "dry-run" flag, -n. Also, removed
    all fixed paths and moved to default configurations.

--- snapshot_hourly.sh
+++ snapshot_hourly.sh
@@ -2,35 +2,102 @@
 
 set -eu
 
-BACKUP_ROOT=/media/rgatti/backup
-FILTER_RULES=
+# Configuration
+#
+BACKUP_ROOT=/media/$USER/backup
+FILTER_RULES_FILE="$HOME/.backup-filter"
+LOG_FILE="$BACKUP_ROOT/hourly.log"
 
-cd $BACKUP_ROOT
+DRY_RUN=false
+RSYNC_ARGS=()
+
+# Print usage and exit.
+#
+usage() {
+	echo "Usage: snapshot_hourly.sh [-n]"
+	echo
+	echo "-n  dry-run"
+	exit 1
+}
+
+# Echo a message if dry-run.
+#
+dry_run_echo() {
+	if $DRY_RUN; then
+		echo "$@"
+	fi
+}
+
+# Echo a message if dry-run and return boolean for conditional.
+#
+# Usage:
+# if do_or_echo "doing something"; then
+#	echo "actually doing something"
+# fi
+#
+do_or_echo() {
+	dry_run_echo "$@"
+	$DRY_RUN && return 1
+	return 0
+}
+
+# Parse script arguments
+if [ $# -eq 1 ]; then
+	[ "${1:-}" == "-n" ] || usage
+	DRY_RUN=true
+	RSYNC_ARGS+=(-n)
+fi
+[ $# -le 1 ] || usage
+
+# Check for filter rules
+if [ -f "$FILTER_RULES_FILE" ]; then
+	RSYNC_ARGS+=(--filter="merge $FILTER_RULES_FILE")
+	if $DRY_RUN; then
+		echo "Using filter rules $FILTER_RULES_FILE"
+		echo "---"
+		cat $FILTER_RULES_FILE
+		echo "---"
+	fi
+fi
+
+# Set log file
+if ! [ -z "$LOG_FILE" ]; then
+	if do_or_echo "Logging to $LOG_FILE"; then
+		RSYNC_ARGS+=(--log-file="$LOG_FILE")
+	fi
+fi
+
+dry_run_echo "Running backup in $BACKUP_ROOT"
 
 # rotate backups
-if [ -d hourly.3 ]; then
-	rm -rf hourly.3
-fi
-if [ -d hourly.2 ]; then
-	mv hourly.2 hourly.3
-fi
-if [ -d hourly.1 ]; then
-	mv hourly.1 hourly.2
-fi
-if [ -d hourly.0 ]; then
-	mv hourly.0 hourly.1
+if [ -d $BACKUP_ROOT/hourly.3 ]; then
+	if do_or_echo "rm -rf hourly.3"; then
+		rm -rf $BACKUP_ROOT/hourly.3
+	fi
 fi
 
-if [ -f "$HOME/.backup-filter" ]; then
-	FILTER_RULES="--filter='merge $HOME/.backup-filter'"
+if [ -d $BACKUP_ROOT/hourly.2 ]; then
+	if do_or_echo "mv hourly.2 hourly.3"; then
+		mv $BACKUP_ROOT/hourly.2 $BACKUP_ROOT/hourly.3
+	fi
+fi
+if [ -d $BACKUP_ROOT/hourly.1 ]; then
+	if do_or_echo "mv hourly.1 hourly.2"; then
+		mv $BACKUP_ROOT/hourly.1 $BACKUP_ROOT/hourly.2
+	fi
+fi
+if [ -d $BACKUP_ROOT/hourly.0 ]; then
+	if do_or_echo "mv hourly.0 hourly.1"; then
+		mv $BACKUP_ROOT/hourly.0 $BACKUP_ROOT/hourly.1
+	fi
 fi
 
-rsync -av --delete                               \
-	--delete-excluded                        \
-	$FILTER_RULES                            \
-	--link-dest=$BACKUP_ROOT/hourly.1        \
-	--log-file=$BACKUP_ROOT/hourly.log       \
-	/home/rgatti/ $BACKUP_ROOT/hourly.0
+rsync "${RSYNC_ARGS[@]}" -av             \
+	--delete --delete-excluded           \
+	--link-dest=$BACKUP_ROOT/hourly.1    \
+	$HOME/ $BACKUP_ROOT/hourly.0
 
 # update mtime
-touch $BACKUP_ROOT/hourly.0
+if do_or_echo "touch $BACKUP_ROOT/hourly.0"; then
+	touch $BACKUP_ROOT/hourly.0
+fi
